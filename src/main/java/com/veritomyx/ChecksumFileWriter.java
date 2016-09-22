b1015ecd898e064bbe712372cb597e3f2e034f50
Adam Tenderholt <adam_tenderholt@targetdiscovery.com>
2016-09-22 13:58:55 -0700

    ChecksumFileWriter: add docstrings
    * Make clear that write(String) doesn't properly handle line separator
      charaters and should use writeln() instead.

--- src/main/java/com/veritomyx/ChecksumFileWriter.java
+++ src/main/java/com/veritomyx/ChecksumFileWriter.java
@@ -7,6 +7,18 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+/**
+ * Used for writing files with checksum that follows the <a
+ * href="https://peakinvestigator.veritomyx.com">guidelines for
+ * PeakInvestigator</a>.
+ * 
+ * <p>
+ * A {@link java.io.FileWriter} wrapped by a {@link java.io.BufferedWriter} is
+ * used.
+ * </p>
+ *
+ * @see https://peakinvestigator.veritomyx.com/api/#FileFormat
+ */
 public class ChecksumFileWriter {
 
 	private BufferedWriter bufferedWriter;
@@ -25,6 +37,14 @@ public class ChecksumFileWriter {
 		output = "";
 	}
 
+	/**
+	 * Write a string to the checksum'd file. Note that this version does not
+	 * properly handle line separators (e.g. '\r' and/or '\n' characters). Use
+	 * {@link #writeln(String msg)} instead.
+	 * 
+	 * @param msg
+	 *            String to be written.
+	 */
 	public void write(String msg) throws IOException {
 		bufferedWriter.write(msg);
 		if (output.length() == 0) {
@@ -33,13 +53,19 @@ public class ChecksumFileWriter {
 			output = output + msg;
 		}
 	}
-	
+
 	public void newLine() throws IOException {
 		bufferedWriter.newLine();
 		fileChecksum.append(output);
 		output = "";
 	}
 
+	/**
+	 * Write a string to the checksum'd file, followed by a new line.
+	 * 
+	 * @param msg
+	 *            String to be written.
+	 */
 	public void writeln(String msg) throws IOException {
 		write(msg);
 		newLine();
